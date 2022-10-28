e68c478b77f4efd869369f26a2a0e0c7caaba439
waterlens <waterlens@waterlens.moe>
2022-10-29 01:40:31 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function compile()
+{
+    backends=(js chez-callcc chez-monadic chez-lift)
+    for elem in "${backends[@]}"
+    do
+        echo "effekt.sh --backend $elem -c $1 -o \"out-$elem\""
+        effekt.sh --backend $elem -c $1 -o "out-$elem"
+    done
+}
+
+compile counter.effekt
+compile nqueens.effekt
+compile nqueens.effekt
