73c827300192d24885b51b454c8bc82a26382db3
waterlens <waterlens@waterlens.moe>
2022-11-02 21:32:03 +0800

    upload almost all benchmarks

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function compile()
+function effekt_compile()
 {
     backends=(js chez-callcc chez-monadic chez-lift)
     for elem in "${backends[@]}"
@@ -10,8 +10,23 @@ function compile()
     done
 }
 
-compile counter.effekt
-compile layered.effekt
-compile nqueens.effekt
-compile fsize.effekt
-compile logger.effekt
+function koka_compile()
+{
+    echo "koka -c $1 --builddir='out-koka'"
+    koka -c $1 --builddir='out-koka'
+}
+
+koka_compile   counter.kk
+koka_compile   layered.kk
+koka_compile   nqueens.kk
+koka_compile   fsize.kk
+koka_compile   logger.kk
+koka_compile   pingpong.kk
+koka_compile   iter.kk
+effekt_compile counter.effekt
+effekt_compile layered.effekt
+effekt_compile nqueens.effekt
+effekt_compile fsize.effekt
+effekt_compile logger.effekt
+effekt_compile pingpong.effekt
+effekt_compile iter.effekt
