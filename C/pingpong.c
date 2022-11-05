f334c0029d290f790531fbc52b26ffd2c65a591e
Cong Ma <cong.ma@uwaterloo.ca>
2022-11-05 11:09:38 -0400

    Done: pingpong.c with two ping_handler

--- C/pingpong.c
+++ C/pingpong.c
@@ -6,7 +6,7 @@ static long longjmp_ret;
 
 struct closure_env_0_arg_1 {
     void **env;
-    long (*f_ptr)(void*, void*);
+    long (*f_ptr)(void*);
 };
 
 struct closure_env_0_arg_2 {
@@ -15,36 +15,45 @@ struct closure_env_0_arg_2 {
 };
 
 
-long pinger(struct closure_env_0_arg_2*, long i, long n);
+long pinger(struct closure_env_0_arg_1*, long i, long n);
 
-long ponger(struct closure_env_0_arg_2*, struct closure_env_0_arg_1*);
+long ponger(struct closure_env_0_arg_2*);
 
-long pong_handler(struct closure_env_0_arg_2* l_ping, void **env) {
+long pong_handler(struct closure_env_0_arg_1* l_ping, void **env) {
     long i = (long)env[0];
     long n = (long)env[1];
     return pinger(l_ping, i+1, n);
 }
 
-long ping_handler(struct closure_env_0_arg_2* l_ping, struct closure_env_0_arg_1* l_pong) {
-    return ponger(l_ping, l_pong);
+long ping_handler(struct closure_env_0_arg_2* l_pong) {
+    return ponger(l_pong);
 }
 
-long pinger(struct closure_env_0_arg_2* l_ping, long i, long n) {
+long pinger(struct closure_env_0_arg_1* l_ping, long i, long n) {
+#ifdef DEBUG
+    printf("pinger %ld %ld\n", i, n);
+#endif // DEBUG
+
     if (!(i < n)) {
         return 0;
     }
     void* pong_handler_env[2] = {(void*)i, (void*)n};
-    struct closure_env_0_arg_1 pong_handler_closure = {pong_handler_env, &pong_handler};
+    struct closure_env_0_arg_2 pong_handler_closure = {pong_handler_env, &pong_handler};
 
-    return (long)l_ping->f_ptr(l_ping, &pong_handler_closure);
+    return (long)l_ping->f_ptr(&pong_handler_closure);
 }
 
-long ponger(struct closure_env_0_arg_2* l_ping, struct closure_env_0_arg_1* l_pong) {
-    return (long)l_pong->f_ptr(l_ping, l_pong->env);
+long ponger(struct closure_env_0_arg_2* l_pong) {
+#ifdef DEBUG
+    printf("ponger\n");
+#endif // DEBUG
+    struct closure_env_0_arg_1 ping_handler_closure = {NULL, &ping_handler};
+
+    return (long)l_pong->f_ptr(&ping_handler_closure, l_pong->env);
 }
 
 int main(void) {
-    struct closure_env_0_arg_2 ping_handler_closure = {NULL, &ping_handler};
+    struct closure_env_0_arg_1 ping_handler_closure = {NULL, &ping_handler};
 
     return pinger(&ping_handler_closure, 0, 10);
 }
